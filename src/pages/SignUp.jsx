2255f869e40138dc123ee80e7c148a9235d030f7
WisdomLota <ngaloru2019@gmail.com>
2024-03-15 18:00:36 +0300

    Did the validation for passwords and security question but i have not yet completed SignUpFormC because i Have not yet done the validation for accepting terms and conditions. Commiting at 6.00pm 15th March 2024

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import SignUpFormA from "../components/SignUpFormA";
 import SignUpFormB from "../components/SignUpFormB";
+import SignUpFormC from "../components/SignUpFormC";
 import SignUpTester from "../components/SignUpTester"
 
 const SignUp = () => {
@@ -8,6 +9,7 @@ const SignUp = () => {
         <SignUpFormA />
         {/* i do not know how to link pages so they will be placed on top of each other */}
         <SignUpFormB />
+        <SignUpFormC />
         {/* <SignUpTester />*/}
         {/*I use the SignUpTester to try out certain things */}
         </>
